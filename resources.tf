5ed007ab4bbcda22df51a0fee9399cc0486bdf29
root <lqiwenn98@gmail.com>
2023-07-13 07:59:22 +0000

    second commit

--- resources.tf
+++ resources.tf
@@ -3,7 +3,7 @@ resource "docker_image" "bgg-database" {
     name = "chukmunnlee/bgg-database:${var.database_version}"
 }
 
-resource "docker_image" "bgg-backed" {
+resource "docker_image" "bgg-backend" {
     name = "chukmunnlee/bgg-backend:${var.backend_version}"
 }
 
